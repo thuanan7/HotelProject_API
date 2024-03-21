7e88d850b1032e2f445aa4935c3627d0f900bfc6
thuanan7 <thuanan7@gmail.com>
2024-03-21 21:16:28 +0700

    Base Service API

---
+++ HotelProject_Web/Services/BaseService.cs
@@ -0,0 +1,66 @@
+﻿using HotelProject_Utility;
+using HotelProject_Web.Models;
+using HotelProject_Web.Services.IServices;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace HotelProject_Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new APIResponse();
+            this.httpClient = httpClient;
+        }
+
+
+        public async Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("HotelProject");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8, "application/json");
+                }
+                switch (apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                HttpResponseMessage apiResponse = await client.SendAsync(message);
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                return APIResponse;
+            }
+            catch (Exception ex)
+            {
+                var dto = new APIResponse
+                {
+                    ErrorMessage = ex.Message,
+                    IsSuccess = false,
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+                return APIResponse;
+            }
+        }
+    }
+}
---
+++ HotelProject_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using HotelProject_Web.Models;
+
+namespace HotelProject_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
